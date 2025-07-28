f2bf3201b9167989f8ae18f32618d61be4e7341f
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-07-28 10:40:21 +0200

    Protect with signed URI instead of upload dir

--- src/Controller/TusController.php
+++ src/Controller/TusController.php
@@ -7,38 +7,38 @@ namespace Terminal42\TusBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Path;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\UriSigner;
 use Symfony\Component\Routing\Attribute\Route;
 use TusPhp\Cache\Cacheable;
 use TusPhp\Tus\Server;
 
-#[Route('/_terminal42_tus/{key}/', name: 'terminal42_tus_post')]
-#[Route('/_terminal42_tus/{key}/{token?}', name: 'terminal42_tus', requirements: ['token' => '.+'])]
+#[Route('/_terminal42_tus/{token?}', name: 'terminal42_tus', requirements: ['token' => '.+'])]
 class TusController extends AbstractController
 {
     public function __construct(
+        private readonly UriSigner $uriSigner,
         #[Autowire('@terminal42_tus.cache')] private readonly Cacheable $cacheAdapter,
-        private readonly Filesystem $filesystem,
         private readonly EventDispatcherInterface $eventDispatcher,
         #[Autowire(param: 'terminal42_tus.upload_dir')] private readonly string $uploadDir,
     ) {
     }
 
-    public function __invoke(string $key): Response
+    public function __invoke(Request $request, string|null $token = null): Response
     {
-        $uploadDir = Path::join($this->uploadDir, $key);
+        if (!$token && !$this->uriSigner->checkRequest($request)) {
+            return new Response('URI must be signed.', Response::HTTP_FORBIDDEN);
+        }
 
-        if (!$this->filesystem->exists($uploadDir)) {
-            throw new NotFoundHttpException();
+        if ($request->query->getInt('expires', PHP_INT_MAX) < time()) {
+            return new Response('URI has expired.', Response::HTTP_FORBIDDEN);
         }
 
         $server = new Server($this->cacheAdapter);
         $server->setDispatcher($this->eventDispatcher);
-        $server->setApiPath($this->generateUrl('terminal42_tus', ['key' => $key]));
-        $server->setUploadDir($uploadDir);
+        $server->setApiPath($this->generateUrl('terminal42_tus'));
+        $server->setUploadDir($this->uploadDir);
 
         return $server->serve();
     }
--- src/Widget/Upload.php
+++ src/Widget/Upload.php
@@ -7,10 +7,7 @@ namespace Terminal42\TusBundle\Widget;
 use Contao\System;
 use Contao\UploadableWidgetInterface;
 use Contao\Widget;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Path;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Uid\Uuid;
 
 class Upload extends Widget implements UploadableWidgetInterface
 {
@@ -24,21 +21,19 @@ class Upload extends Widget implements UploadableWidgetInterface
      */
     protected $strTemplate = 'be_widget';
 
-    public function generate()
+    public function generate(): string
     {
         $container = System::getContainer();
 
         $GLOBALS['TL_JAVASCRIPT'][] = $container->get('assets.packages')->getUrl('tus.js', 'terminal42_tus');
         $GLOBALS['TL_CSS'][] = $container->get('assets.packages')->getUrl('tus.css', 'terminal42_tus');
 
-        $key = (string) Uuid::v1();
-
-        (new Filesystem())->mkdir(Path::join(System::getContainer()->getParameter('terminal42_tus.upload_dir'), $key));
+        $uriSigner = $container->get('uri_signer');
+        $endpoint = $container->get('router')->generate('terminal42_tus', ['expires' => strtotime('+1 hour')], UrlGeneratorInterface::ABSOLUTE_URL);
 
         return $container->get('twig')->render('@Contao/backend/terminal42_tus_upload.html.twig', [
             'name' => $this->name,
-            'key' => $key,
-            'endpoint' => $container->get('router')->generate('terminal42_tus_post', ['key' => $key], UrlGeneratorInterface::ABSOLUTE_URL),
+            'endpoint' => $uriSigner->sign($endpoint),
         ]);
     }
 
